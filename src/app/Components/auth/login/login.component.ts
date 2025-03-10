e8f4a43a644fba31500f0bf43788504da30e3eca
Fábio Silva <121696024+FabioTorresSilva@users.noreply.github.com>
2025-03-10 14:32:36 +0000

    login working

--- src/app/Components/auth/login/login.component.ts
+++ src/app/Components/auth/login/login.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../Services/auth.service';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Route, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService , private router:Router) { }
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        console.log('Login Successful:', response);
+
+        // Store the full response in localStorage
+        localStorage.setItem('user', JSON.stringify(response));
+
+        // Redirect to home page
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.error('Login Failed:', error);
+        alert('Login falhou. Verifique suas credenciais.');
+      }
+    });
+  }
 
 }
